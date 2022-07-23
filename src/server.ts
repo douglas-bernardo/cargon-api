2fae4a776c7ae380897ad3ad7ec932d9b57ee5a9
Douglas Bernardo <jkdouglas21@gmail.com>
2022-07-23 18:15:34 -0300

    add eslint, prettier and editorConfig to project

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import express from 'express';
 const port = 3333;
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send("Hello World Jackson");
+app.get('/', (request, response) => {
+  return response.send('Hello World Jackson');
 });
 
 app.listen(port, () => {
-    return `Server on port ${port}`;
+  return `Server on port ${port}`;
 });
